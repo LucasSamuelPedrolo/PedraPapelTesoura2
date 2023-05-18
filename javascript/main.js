880ad723fd4e2d8ef89c454f39c04191ec96377b
Lucas Pedrolo <132529241+LucasSamuelPedrolo@users.noreply.github.com>
2023-05-18 00:11:38 -0300

    melhorias no js e ajustes de funções

--- javascript/main.js
+++ javascript/main.js
@@ -1,90 +1,69 @@
-(() => {
+function JokenpoGame() {
 
-    //start game
-    const iniciar = () => {
+    //coloca um timer na tela para jogar
+    function timerJogada() {
+        const timerHold = document.querySelector('.timerHolding');
+        const timerSelect = timerHold.querySelector('.timer');
+        let timerCount = 1;
 
-        const telaStart = document.querySelector('.container');
-        const btnStart = document.querySelector('.play');
-
-        btnStart.addEventListener('click', () => {
-
-            btnStart.classList.add('playClicked');
-            telaStart.style.animation = 'escureceTela 2s 1s'
-
-            //esconder a tela de inicio
-            setTimeout(() => {
-                telaStart.style.visibility = 'hidden';
-                btnStart.classList.remove('playClicked');
-            }, 3000
-            );
-        });
-    };
-    //comando prioritario, nao sobrepor 
-    iniciar();
-
-    const detectaClick = () => {
-        document.addEventListener('click', (e) => {
-
-            const opcoes = ['pedra', 'papel', 'tesoura'];
-            const el = e.target;
-            let jogadaMaquina = Math.floor(Math.random() * opcoes.length);
-
-            if (el.classList.contains('papelSelect')) {
-                comparativoJogadas('papel', opcoes[jogadaMaquina])
-            }
-            if (el.classList.contains('pedraSelect')) {
-                comparativoJogadas('pedra', opcoes[jogadaMaquina])
-            }
-            if (el.classList.contains('tesouraSelect')) {
-                comparativoJogadas('tesoura', opcoes[jogadaMaquina])
-            }
-        })
-    }
-
-    detectaClick()
-
-    const timerTela = () => {
-        let teste;
-        const timerHtml = document.querySelector('.timer');
-        let num = 1;
-
-        teste = setInterval(() => {
-
-            timerHtml.innerHTML = `<p class = animationTimer> ${++num} </p>`;
-        }, 2500);
+        const timerStart = setInterval(() => {
+            timerSelect.innerHTML = `<p class=animationTimer>${++timerCount}</p>`;
+        }, 2000);
 
         setTimeout(() => {
-            clearInterval(teste);
-            timerHtml.style.visibility = 'hidden';
-        }, 7300);
+            clearInterval(timerStart)
+            timerHold.style.visibility = 'hidden';
+        }, 6000);
+
     }
 
-    const comparativoJogadas = (player, maquina) => {
-        switch (true) {
-            case (player === 'papel' && maquina === 'tesoura'):
-                console.log('maquina venceu jogando tesoura');
-                break;
-
-            case (player === 'pedra' && maquina === 'papel'):
-                console.log('maquina venceu jogando papel');
-                break;
-
-            case (player === 'tesoura' && maquina === 'pedra'):
-                console.log('maquina Venceu jogando pedra');
-                break;
-
-            case (player === maquina):
-                console.log('empate');
-                break;
-
-            default:
-                console.log('player venceu')
-                break;
-        }
+    function inputMaquina() {
+        const maquinaOpcoes = ['papel', 'pedra', 'tesoura'];
+        return maquinaOpcoes[Math.floor(Math.random() * maquinaOpcoes.length)];
     }
 
-    const jogaCarta = jogada =>{
+    function centerCardClone(value) {
+        
         
     }
 
-})();
+    this.start = () => {
+        this.retiraTelaStart();
+        this.inputCardPlayer();
+    },
+
+        this.retiraTelaStart = () => {
+            const containerTelaStart = document.querySelector('.container');
+            const btnStart = containerTelaStart.querySelector('.play');
+
+            //pequenos efeitos de tela e retirada da tela de 'jogar'
+            btnStart.addEventListener('click', () => {
+                containerTelaStart.style.animation = 'escureceTela 2s';
+
+                setTimeout(() => {
+                    containerTelaStart.style.visibility = 'hidden'
+                }, 2000)
+            });
+
+        },
+
+        this.inputCardPlayer = () => {
+            const escolhasJogador = {
+                papelSelect: 'papel', pedraSelect: 'pedra', tesouraSelect: 'tesoura'
+            };
+            document.addEventListener('click', (e) => {
+                const el = e.target;
+                if (escolhasJogador[el.id]) {
+                    const maquinaChose = inputMaquina();
+
+                    animacaoCards(el.id)
+                    console.log('jogadas :', escolhasJogador[el.id], maquinaChose);
+                }
+            })
+        }
+
+}
+
+const jokenpoStart = new JokenpoGame();
+jokenpoStart.start();
+
