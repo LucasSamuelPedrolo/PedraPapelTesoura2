0c23b2fbc70e88e1afd2ae85b932cfed163d51f8
Lucas Pedrolo <132529241+LucasSamuelPedrolo@users.noreply.github.com>
2023-05-31 23:29:43 -0300

    adicionando timer

--- javascript/main.js
+++ javascript/main.js
@@ -4,34 +4,40 @@ const opcoes = ['papel', 'pedra', 'tesoura'];
 
 for (let i = 0; i < card.length; i++) {
     card[i].addEventListener('click', () => {
-        resultado(opcoes[i], maqRandom())
-        cloneCard(opcoes[i])
+        resultado(opcoes[i], maqRandom());
+        cloneCard(opcoes[i], 'player');
     })
 }
 
 function maqRandom() {
     const escolhaAleatoria = Math.floor(Math.random() * opcoes.length);
+    cloneCard(opcoes[escolhaAleatoria], 'maquina');
     return opcoes[escolhaAleatoria];
 }
 
 function resultado(player, maquina) {
+
     if (player === maquina) {
+        timer('empate')
         console.log('empate');
         return;
     }
     if (player === 'papel' && maquina === 'tesoura') {
+        timer('Maquina')
         placar(0, 1);
         return;
     }
     if (player === 'tesoura' && maquina === 'pedra') {
+        timer('Maquina')
         placar(0, 1);
         return;
     }
     if (player === 'pedra' && maquina === 'papel') {
+        timer('Maquina')
         placar(0, 1);
         return;
     }
-
+    timer('Você venceu :D')
     placar(1, 0);
 }
 
@@ -60,7 +66,7 @@ function placar(player, maquina) {
 
 //efeitos ou animações de tela abaixo..
 
-function cloneCard(obj) {
+function cloneCard(obj, user) {
     const cardsImg = {
         papel: 'css/image/papel.jpg',
         pedra: 'css/image/pedraCartoon.jfif',
@@ -74,8 +80,47 @@ function cloneCard(obj) {
         cardClone.appendChild(criarElemento);
     };
     cardClone.classList.add('gameCardClone');
-    cardClone.style.bottom = '30%'
+    cardClone.querySelector('img').src = cardsImg[obj];
 
-    const selectImgCardClone = cardClone.querySelector('img').src = cardsImg[obj];
+
+    if (user === 'maquina') {
+        const cardCloneMaq = document.getElementById('holdCardMaq');
+        if (!(cardCloneMaq.hasChildNodes())) {
+            cardCloneMaq.appendChild(criarElemento);
+        };
+
+        cardCloneMaq.classList.add('gameCardClone');
+        cardCloneMaq.querySelector('img').src = cardsImg[obj];
+        cardCloneMaq.style.top = '30%';
+        return;
+    }
+
+    cardClone.style.bottom = '30%';
+
+}
+
+function timer(pontuador) {
+    let tempo = 4;
+    const timerHolding = document.querySelector('.timerHolding')
+    const timerCounter = timerHolding.querySelector('.timer');
+
+    timerHolding.style.visibility = 'visible';
+
+    const vencedor = timerHolding.querySelector('.vencedorDoRound');
+    vencedor.innerHTML = 'Vencedor desse Round : <br>';
+
+    vencedor.innerHTML += pontuador;
+
+    const temporizador = setInterval(() => {
+        tempo--
+        timerCounter.innerHTML = `<p class="animationTimer">${tempo}</p>`;
+
+        if (tempo === 0) {
+            tempo = 4;
+            clearInterval(temporizador);
+            timerHolding.style.visibility = 'hidden';
+        }
+    }, 2000);
+
 
 }
